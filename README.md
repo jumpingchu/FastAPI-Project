45130c66051dec7f17615eb5dda39738a993611a
unknown <jiaping.chu@walkermedia.tw>
2020-12-30 16:01:49 +0800

    使用 Templates

--- README.md
+++ README.md
@@ -1,24 +1,61 @@
 # FastAPI-Project
-【FastAPI】實作測試
 
 ## 基本操作
+
 ### 官網
 * https://fastapi.tiangolo.com
 
 ### 安裝
-
 	$ pip install fastapi
 	$ pip install uvicorn
 
 ### 執行
 * 創建 `main.py` 後，執行以下指令
-	
-		$ uvicorn main:app --reload
+```bash
+$ uvicorn main:app --reload
+```
 
-
-
-## 特色
+### 特色
 * 可 Query item：`http://127.0.0.1:8000/items/5?q=somequery`
-> {"item_id": 5, "q": "somequery"}
+* 返回物件為 {"item_id": 5, "q": "somequery"}
 * 支援 Swagger UI 測試介面：`http://127.0.0.1:8000/docs`
 * 支援異步調用
+
+---
+
+## 使用 Templates
+### 參考資料: 
+* https://fastapi.tiangolo.com/advanced/templates/
+
+### 安裝 Jinja2
+	$ pip install jinja2
+
+### 需另外匯入模組
+```python 
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+```
+
+### 注意事項
+
+```python
+# path operation 要加入 Request 參數
+response_class=HTMLResponse
+```
+```python
+# 返回的是 TemplateResponse
+return templates.TemplateResponse("template_file", context)
+```
+* context 為 `key : value` 形式
+* HTML Templates 的變數要用 Jinja2 的 `{{ context_key }}` 來表示
+
+---
+
+## 功能
+
+### ver 0.1 (2020/12/30)
+* 根目錄有前往各功能的按鈕
+* 點擊後會出現該分類的最新一篇文章
+
+---
--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import requests
+import re
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
+base_url = 'https://www.popdaily.com.tw/'
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    url_japan_new = 'http://localhost:8000/popdaily/japan/new'
+    url_travel_new = 'http://localhost:8000/popdaily/travel/new'
+    url_food_new = 'http://localhost:8000/popdaily/food/new'
+    url_life_new = 'http://localhost:8000/popdaily/life/new'
+    url_press_new = 'http://localhost:8000/popdaily/press/new'
+    return_dict = {
+        "request": request,
+        "btn_japan": url_japan_new,
+        "btn_travel": url_travel_new,
+        "btn_food": url_food_new,
+        "btn_life": url_life_new,
+        "btn_press": url_press_new
+    }
+    return templates.TemplateResponse("home.html", return_dict)
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/popdaily/{article_type}/new", response_class=HTMLResponse)
+def popdaily_latest_urls(article_type: str, request: Request):
+    page_url = base_url + article_type
+    resp = requests.get(page_url)
+    pattern = r'(?<=,\"url\":\")https://www.popdaily.com.tw/{}/\d+'.format(article_type)
+    if resp.status_code == 200:
+        urls = re.findall(pattern , resp.text)
+    return templates.TemplateResponse("item.html", {"request": request, "urls": urls})
